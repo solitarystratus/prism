860131fc4d2acac65ce41aa10335b684a080e639
Nazia Tasnim Khanam <149527494+solitarystratus@users.noreply.github.com>
2026-08-18 20:01:32 +0600

    Delete src/visuals/region-effects.js

--- src/visuals/region-effects.js
+++
@@ -1,7 +0,0 @@
-export function regionAtmosphere(region, levelNumber) {
-  const shimmer = ((levelNumber * 17) % 13) / 13;
-  return {
-    hue: region.hue + Math.round((shimmer - .5) * 6),
-    rain: Math.max(.66, Math.min(.94, region.rain + (shimmer - .5) * .04))
-  };
-}
